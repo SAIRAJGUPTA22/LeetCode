8d29a7f1236a8e676cfaf22c68af36d74f8be10b
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2023-10-15 14:59:01 +0530

    Time: 1711 ms (80.29%) | Memory: 0B (100.00%) - LeetSync

--- 1301-reformat-department-table/reformat-department-table.sql
+++ 1301-reformat-department-table/reformat-department-table.sql
@@ -1,24 +1,16 @@
-/* Write your T-SQL query statement below */
 SELECT
     id,
-    [Jan] AS Jan_Revenue,
-    [Feb] AS Feb_Revenue,
-    [Mar] AS Mar_Revenue,
-    [Apr] AS Apr_Revenue,
-    [May] AS May_Revenue,
-    [Jun] AS Jun_Revenue,
-    [Jul] AS Jul_Revenue,
-    [Aug] AS Aug_Revenue,
-    [Sep] AS Sep_Revenue,
-    [Oct] AS Oct_Revenue,
-    [Nov] AS Nov_Revenue,
-    [Dec] AS Dec_Revenue
-FROM (
-    SELECT id, revenue, month FROM department
-) s
-PIVOT (
-    SUM(revenue)
-    FOR Month IN (
-        [Jan],[Feb],[Mar],[Apr],[May],[Jun],
-        [Jul],[Aug],[Sep],[Oct],[Nov],[Dec] )
-) p;
+    SUM(CASE WHEN Month = 'Jan' THEN Revenue ELSE null END) AS Jan_Revenue,
+    SUM(CASE WHEN Month = 'Feb' THEN Revenue ELSE null END) AS Feb_Revenue,
+    SUM(CASE WHEN Month = 'Mar' THEN Revenue ELSE null END) AS Mar_Revenue,
+    SUM(CASE WHEN Month = 'Apr' THEN Revenue ELSE null END) AS Apr_Revenue,   
+    SUM(CASE WHEN Month = 'May' THEN Revenue ELSE null END) AS May_Revenue,
+    SUM(CASE WHEN Month = 'Jun' THEN Revenue ELSE null END) AS Jun_Revenue,
+    SUM(CASE WHEN Month = 'Jul' THEN Revenue ELSE null END) AS Jul_Revenue,
+    SUM(CASE WHEN Month = 'Aug' THEN Revenue ELSE null END) AS Aug_Revenue, 
+    SUM(CASE WHEN Month = 'Sep' THEN Revenue ELSE null END) AS Sep_Revenue,
+    SUM(CASE WHEN Month = 'Oct' THEN Revenue ELSE null END) AS Oct_Revenue,
+    SUM(CASE WHEN Month = 'Nov' THEN Revenue ELSE null END) AS Nov_Revenue,
+    SUM(CASE WHEN Month = 'Dec' THEN Revenue ELSE null END) AS Dec_Revenue       
+FROM Department
+GROUP BY id;
