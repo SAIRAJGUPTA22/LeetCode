aae171f740a6d2ca3571e69aa2be08b52feb9c8c
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2023-10-17 22:22:08 +0530

    Time: 881 ms (95.69%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,14 +1,4 @@
-SELECT A.month, A.country, A.trans_count,CASE WHEN approved_count>0 THEN approved_count ELSE 0 END AS approved_count, A.trans_total_amount,CASE WHEN approved_total_amount >0 THEN approved_total_amount ELSE 0 END AS  
-approved_total_amount
-FROM (
-    SELECT SUBSTRING(CONVERT(VARCHAR, trans_date, 120), 1, 7) AS month, country, COUNT(id) AS trans_count, SUM(amount) AS trans_total_amount
-    FROM Transactions
-    GROUP BY SUBSTRING(CONVERT(VARCHAR, trans_date, 120), 1, 7), country
-) A
-LEFT JOIN (
-    SELECT SUBSTRING(CONVERT(VARCHAR, trans_date, 120), 1, 7) AS month, country, COUNT(id) AS approved_count, SUM(amount) AS approved_total_amount
-    FROM Transactions
-    WHERE State = 'approved'
-    GROUP BY SUBSTRING(CONVERT(VARCHAR, trans_date, 120), 1, 7), country
-) B
-ON A.month = B.month AND A.country = B.country;
+# Write your MySQL query statement below
+SELECT  SUBSTR(trans_date,1,7) as month, country, count(id) as trans_count, SUM(CASE WHEN state = 'approved' then 1 else 0 END) as approved_count, SUM(amount) as trans_total_amount, SUM(CASE WHEN state = 'approved' then amount else 0 END) as approved_total_amount
+FROM Transactions
+GROUP BY month, country
